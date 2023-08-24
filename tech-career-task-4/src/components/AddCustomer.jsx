3f9e2124de79371d79bec0afb70b4c3971b8b217
Guney Unay <unaygney17@gmail.com>
2023-08-24 12:19:09 +0300

    custommer addition function added

---
+++ tech-career-task-4/src/components/AddCustomer.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+
+function AddCustomer() {
+  const [newCustomer, setNewCustomer] = useState({
+    companyName: "",
+    contactName: "",
+    contactTitle: "",
+
+  });
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+ 
+    try{
+      const res = await axios.post('https://northwind.vercel.app/api/customers' , newCustomer)
+      console.log("Response" , res.data)
+
+      setNewCustomer({
+        companyName: "",
+        contactName: "",
+        contactTitle: "",
+
+      })
+    } catch(error){
+      console.log("Error" , error)
+    }
+
+
+  };
+
+const handleChange = (e) =>{
+setNewCustomer({...newCustomer , [e.target.name] : e.target.value})
+}
+
+
+
+
+  return (
+    <>
+      <nav>
+        <NavLink to="/addcustomer">Add Customer</NavLink>
+        <NavLink to="/">Add Customer</NavLink>
+      </nav>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="companyName">Company Name</label>
+          <input
+            value={newCustomer.companyName}
+            name="companyName"
+            id="companyName"
+            type="text"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="contactName">Contact Name</label>
+          <input
+            value={newCustomer.contactName}
+            name="contactName"
+            id="contactName"
+            type="text"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="contactTitle">Contact Title</label>
+          <input
+            value={newCustomer.contactTitle}
+            name="contactTitle"
+            id="contactTitle"
+            type="text"
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Add Customer</button>
+      </form>
+    </>
+  );
+}
+
+export default AddCustomer;
---
+++ tech-career-task-4/src/components/Index.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { NavLink } from 'react-router-dom';
+
+
+function Index() {
+
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [sortDirection, setSortDirection] = useState("ascending");
+    const [view, setView] = useState(10);
+  
+    useEffect(() => {
+      const getData = () => {
+        axios
+          .get("https://northwind.vercel.app/api/customers")
+          .then((response) => {
+            setProducts(response.data.slice(0, view));
+          })
+          .catch((error) => {
+            console.error("error: ", error);
+          })
+  
+          .finally(() => {
+            setIsLoading(false);
+          });
+      };
+  
+      getData();
+    }, [view]);
+  
+    const handleSort = () => {
+      const sortedProducts = [...products];
+      if (sortDirection === "ascending") {
+        sortedProducts.sort((a, b) => a.companyName.localeCompare(b.companyName));
+        setSortDirection("descending");
+      } else {
+        sortedProducts.sort((a, b) => b.companyName.localeCompare(a.companyName));
+        setSortDirection("ascending");
+      }
+      setProducts(sortedProducts);
+    };
+  
+    const handleNumberChange = (event) => {
+      setView(parseInt(event.target.value));
+    };
+  return (
+    
+    <div>
+        <>
+      {(isLoading && <div>Loading</div>) || (
+        <>
+
+        <nav>
+
+          <NavLink to="/">Home Page</NavLink>
+          <NavLink to="/addcustomer">Add Customer</NavLink>
+        </nav>
+          <header>
+            <h1>Customer Page</h1>
+
+            <label htmlFor="numberData">View : </label>
+            <select id="numberData" value={view} onChange={handleNumberChange}>
+              {" "}
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+            </select>
+          </header>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th onClick={handleSort}>Company Name</th>
+                <th>Street</th>
+                <th>City</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>{product.companyName}</td>
+                  <td>{product.address?.street}</td>
+                  <td>{product.address?.city}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
+    </div>
+  )
+}
+
+export default Index
